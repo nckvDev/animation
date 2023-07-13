6b22a77a4add42c5030f70f33712d4a7a15dc792
nckvDev <jirapong.ku@rmuti.ac.th>
2023-07-13 11:08:49 +0700

    feat(:sparkles:): Section Intro

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from 'next/link'
 
 export default function Home() {
    return (
       <main>
          <h1>Hello World!</h1>
+         <Link href='/smooth-scroll'>Smooth Scroll</Link>
       </main>
    )
 }
---
+++ components/Description/index.js
@@ -0,0 +1,7 @@
+import styles from './style.module.css'
+
+const Description = () => {
+   return <div className={styles.description}>Description</div>
+}
+
+export default Description
